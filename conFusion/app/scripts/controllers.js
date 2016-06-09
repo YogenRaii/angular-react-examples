8a6b2e33b06603414b5987503bd5bf671f1c6e4c
Yogen'sWork <ygnrai@gmail.com>
2016-06-09 17:57:17 -0500

    added  for client server communication

--- conFusion/app/scripts/controllers.js
+++ conFusion/app/scripts/controllers.js
@@ -56,7 +56,7 @@ angular.module('confusionApp')
                         
         }])
 
-        .controller('FeedbackController', ['$scope', function($scope) {
+        .controller('FeedbackController', ['$scope','feedbackFactory' ,function($scope,feedbackFactory) {
             
             $scope.sendFeedback = function() {
                 
@@ -67,11 +67,17 @@ angular.module('confusionApp')
                     console.log('incorrect');
                 }
                 else {
-                    $scope.invalidChannelSelection = false;
-                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
-                    $scope.feedback.mychannel="";
-                    $scope.feedbackForm.$setPristine();
-                    console.log($scope.feedback);
+                    feedbackFactory.getFeedbacks().save($scope.feedback,function(response){
+                        $scope.invalidChannelSelection = false;
+                        $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                        $scope.feedback.mychannel="";
+                        $scope.feedbackForm.$setPristine();
+                        console.log(response);
+                    },function(response){  //incase of failure while saving
+                        console.log(response);
+                    });
+
+                    
                 }
             };
         }])
@@ -115,7 +121,14 @@ angular.module('confusionApp')
 
         // implement the IndexController and About Controller here
         .controller('IndexController',['$scope','menuFactory','corporateFactory',function($scope,menuFactory,corporateFactory){
-            $scope.promotion = menuFactory.getPromotion(0);
+            $scope.showPromotion = false;
+            $scope.promotion = menuFactory.getPromotion().get({id:0}).$promise.then(
+                function(response){
+                    $scope.promotion = response;
+                    $scope.showPromotion = true;
+                },function(response){
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                });
             $scope.showDish = false;
             $scope.message = "Loading...";
             
@@ -129,11 +142,25 @@ angular.module('confusionApp')
                             }
                         );
 
-            $scope.leader = corporateFactory.getLeader(3);
+            $scope.showLeader = false;
+            $scope.leader = corporateFactory.getLeaders().get({id:3}).$promise.then(
+                function(response){
+                    $scope.leader = response;
+                    $scope.showLeader = true;
+                },function(response){
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                });
         }])
 
         .controller('AboutController',['$scope','corporateFactory',function($scope,corporateFactory){
-                $scope.leaders = corporateFactory.getLeaders();
+                $scope.showLeader = false;
+                $scope.leaders = corporateFactory.getLeaders().query(
+                    function(response){
+                        $scope.leaders = response;
+                        $scope.showLeader = true;
+                    },function(response){
+                        $scope.message = "Error: "+response.status + " " + response.statusText;
+                    });
         }])
 
 ;
