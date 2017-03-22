97640f5769ce9258da3fe148ad8b09ea1572b4af
542596 <yogen.rai@delta.com>
2017-03-22 13:21:57 -0400

    survey.js modified to show the hovered star count

--- iflex/app/survey/survey.js
+++ iflex/app/survey/survey.js
@@ -3,62 +3,123 @@
 angular.module('myApp.survey', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/survey', {
-    templateUrl: 'survey/survey-tpl.html',
-    controller: 'SurveyCtrl'
-  });
+	$routeProvider.when('/survey', {
+		templateUrl: 'survey/survey-tpl.html',
+		controller: 'SurveyCtrl'
+	});
 }])
 
 .controller('SurveyCtrl', ['$scope',function($scope) {
-	$scope.starRating1 = 4;
-    $scope.hoverRating1 = 0;
+	$scope.ratings = [
+	{
+		id:1,
+		starRating: 0,
+		title: 'Seat Comfort: '
+	},
+	{
+		id:2,
+		starRating: 0,
+		title: 'On-time Departure: '
+	},
+	{
+		id:3,
+		starRating: 0,
+		title: 'On-time Arrival: '
+	},
+	{
+		id:4,
+		starRating: 0,
+		title: 'Food: '
+	},
+	{
+		id:5,
+		starRating: 0,
+		title: 'In-flight Entertainment(IFE): '
+	},
+	{
+		id:6,
+		starRating: 0,
+		title: 'Rest Room: '
+	},
+	{
+		id:7,
+		starRating: 0,
+		title: 'Flight Attendants:  '
+	},
+	{
+		id:8,
+		starRating: 0,
+		title: 'Overall Cabin Experience: '
+	},
+	{
+		id:9,
+		starRating: 0,
+		title: 'Wifi: '
+	}
 
-    $scope.click1 = function (param) {
-        console.log('Click(' + param + ')');
-    };
+	];
 
-    $scope.mouseHover1 = function (param) {
-        console.log('mouseHover(' + param + ')');
-        $scope.hoverRating1 = param;
-    };
+	$scope.submit = function() {
+		console.log("form submitted...");
+		var passengerResponse = {
+			ratings: $scope.ratings,
+			passengerInfo: $scope.passenger
+		}
+		console.log(passengerResponse);
+	};
 
-    $scope.mouseLeave1 = function (param) {
-        console.log('mouseLeave(' + param + ')');
-        $scope.hoverRating1 = param + '*';
-    };
+	var currentRatings = new Array();
+
+	$scope.ratings.forEach(rating => currentRatings.push(rating.starRating));
+
+	$scope.click = function (param, id) {
+		console.log('Click(' + param + ', ' + id+')');
+		$scope.ratings[id-1].starRating = param;
+		currentRatings[id-1] = param;
+	};
+
+	$scope.mouseHover = function (param, id) {
+		console.log('mouseHover(' + param + ')');
+		$scope.ratings[id-1].starRating = param;
+	};
+
+	$scope.mouseLeave = function (param, id) {
+		console.log('mouseLeave(' + param + ')');
+		$scope.ratings[id-1].starRating = currentRatings[id-1];
+	};
 }])
 
 .directive('starRating', function () {
-    return {
-        scope: {
-            rating: '=',
-            maxRating: '@',
-            readOnly: '@',
-            click: "&",
-            mouseHover: "&",
-            mouseLeave: "&"
-        },
-        restrict: 'EA',
-        template:
-            "<div style='display: inline-block; margin: 0px; padding: 0px; cursor:pointer;' ng-repeat='idx in maxRatings track by $index'> \
-                    <img ng-src='{{((hoverValue + _rating) <= $index) && \"http://www.codeproject.com/script/ratings/images/star-empty-lg.png\" || \"http://www.codeproject.com/script/ratings/images/star-fill-lg.png\"}}' \
-                    ng-Click='isolatedClick($index + 1)' \
-                    ng-mouseenter='isolatedMouseHover($index + 1)' \
-                    ng-mouseleave='isolatedMouseLeave($index + 1)'></img> \
-            </div>",
-        compile: function (element, attrs) {
-            if (!attrs.maxRating || (Number(attrs.maxRating) <= 0)) {
-                attrs.maxRating = '5';
-            };
-        },
-        controller: function ($scope, $element, $attrs) {
-            $scope.maxRatings = [];
+	return {
+		scope: {
+			rating: '=',
+			maxRating: '@',
+			readOnly: '@',
+			click: "&",
+			mouseHover: "&",
+			mouseLeave: "&"
+		},
+		restrict: 'EA',
+		template:
+		"<div style='display: inline-block; margin: 0px; padding: 0px; cursor:pointer;' ng-repeat='idx in maxRatings track by $index'> \
+		<img ng-src='{{((hoverValue + _rating) <= $index) && \"http://www.codeproject.com/script/ratings/images/star-empty-lg.png\" || \"http://www.codeproject.com/script/ratings/images/star-fill-lg.png\"}}' \
+		ng-Click='isolatedClick($index + 1)' \
+		ng-mouseenter='isolatedMouseHover($index + 1)' \
+		ng-mouseleave='isolatedMouseLeave($index + 1)'></img> \
+		</div>",
+		compile: function (element, attrs) {
+			if (!attrs.maxRating || (Number(attrs.maxRating) <= 0)) {
+				attrs.maxRating = '5';
+			};
+		},
+		controller: function ($scope, $element, $attrs) {
+			$scope.maxRatings = [];
 
-            for (var i = 1; i <= $scope.maxRating; i++) {
-                $scope.maxRatings.push({});
-            };
+			for (var i = 1; i <= $scope.maxRating; i++) {
+				$scope.maxRatings.push({});
+			};
 
-            $scope._rating = $scope.rating;
+			$scope._rating = $scope.rating;
 			
 			$scope.isolatedClick = function (param) {
 				if ($scope.readOnly == 'true') return;
@@ -84,11 +145,12 @@ angular.module('myApp.survey', ['ngRoute'])
 				if ($scope.readOnly == 'true') return;
 
 				$scope._rating = $scope.rating;
+				console.log('isolatedMouseLeave(' + $scope._rating + ')');
 				$scope.hoverValue = 0;
 				$scope.mouseLeave({
 					param: param
 				});
 			};
-        }
-    };
+		}
+	};
 });
